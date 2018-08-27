4dd1436affeefa9fb0e0891c6ba0d73a76dad856
JamesRileydev <jamesriley.dev@gmail.com>
2018-08-27 17:18:48 -0400

    added missing semi-colons

--- cryptSolution.js
+++ cryptSolution.js
@@ -1,64 +1,66 @@
 let crypt = ["SEND", "MORE", "MONEY"];
 
 let solution = [['O', '0'],
-                ['M', '1'],
-                ['Y', '2'],
-                ['E', '5'],
-                ['N', '6'],
-                ['D', '7'],
-                ['R', '8'],
-                ['S', '9']];
+['M', '1'],
+['Y', '2'],
+['E', '5'],
+['N', '6'],
+['D', '7'],
+['R', '8'],
+['S', '9']];
 
 
 
 function isCryptSolution(crypt, solution) {
-  let expectedResult = true
-  
+  let expectedResult = true;
+
   solution = solution.reduce((acc, index) => {
-    acc[index[0]] = index[1]
-    return acc
-  }, {})
-  
+    acc[index[0]] = index[1];
+    return acc;
+  }, {});
+
   let result = crypt.map(word => {
-    let num = ''
+    let num = '';
     word.split('').forEach(letter => {
-      num += solution[letter]
+      num += solution[letter];
     })
-    return num
-  })
-  
+    return num;
+  });
+
   if ((+result[0] + +result[1] !== +result[2])) {
-    expectedResult = false
+    expectedResult = false;
   }
-  if ((result[0][0] === '0' || result[0][1] === '0' && result [2][0] === '0'|| result[1][0] === '0' && result[1][1] === '0') && result[0].length !== 1) {
-    expectedResult = false
+  if ((result[0][0] === '0' || result[0][1] === '0' && result[2][0] === '0' || result[1][0] === '0' && result[1][1] === '0') && result[0].length !== 1) {
+    expectedResult = false;
   }
 
-  return expectedResult
-}function isCryptSolution(crypt, solution) {
-  let expectedResult = true
-  
+  return expectedResult;
+}
+
+function isCryptSolution(crypt, solution) {
+  let expectedResult = true;
+
   solution = solution.reduce((acc, index) => {
-    acc[index[0]] = index[1]
-    return acc
-  }, {})
-  
+    acc[index[0]] = index[1];
+    return acc;
+  }, {});
+
   let result = crypt.map(word => {
-    let num = ''
+    let num = '';
     word.split('').forEach(letter => {
-      num += solution[letter]
-    })
-    return num
+      num += solution[letter];
+    });
+    return num;
   })
-  
+
   if ((+result[0] + +result[1] !== +result[2])) {
-    expectedResult = false
+    expectedResult = false;
   }
-  if ((result[0][0] === '0' || result[0][1] === '0' && result [2][0] === '0'|| result[1][0] === '0' && result[1][1] === '0') && result[0].length !== 1) {
-    expectedResult = false
+  if ((result[0][0] === '0' || result[0][1] === '0' && result[2][0] === '0' || result[1][0] === '0' && result[1][1] === '0') && result[0].length !== 1) {
+    expectedResult = false;
   }
 
-  return expectedResult
+  return expectedResult;
 }
 
 console.log(isCryptSolution(crypt, solution));
