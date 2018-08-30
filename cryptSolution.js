ca6aee654a8e3d1052dddf99940c4164bc0e0274
JamesRileydev <jamesriley.dev@gmail.com>
2018-08-29 20:41:59 -0400

    rewrote cryptSolution

--- cryptSolution.js
+++ cryptSolution.js
@@ -1,44 +1,44 @@
 let crypt = ["SEND", "MORE", "MONEY"];
 
-let solution = [['O', '0'],
-['M', '1'],
-['Y', '2'],
-['E', '5'],
-['N', '6'],
-['D', '7'],
-['R', '8'],
-['S', '9']];
+let key = [['O', '0'],
+           ['M', '1'],
+           ['Y', '2'],
+           ['E', '5'],
+           ['N', '6'],
+           ['D', '7'],
+           ['R', '8'],
+           ['S', '9']];
 
 
-
-function isCryptSolution(crypt, solution) {
+function cryptSolution(crypt, key) {
   let expectedResult = true;
 
-  solution = solution.reduce((acc, index) => {
+  let solution = key.reduce((acc, index) => {
     acc[index[0]] = index[1];
     return acc;
   }, {});
 
-  let result = crypt.map(word => {
+  let result = crypt.map(word =>{
     let num = '';
     word.split('').forEach(letter => {
       num += solution[letter];
-    })
+    });
     return num;
   });
 
-  if ((+result[0] + +result[1] !== +result[2])) {
+  if (+result[0] + +result[1] !== +result[2]) {
     expectedResult = false;
   }
-  if ((result[0][0] === '0' || result[0][1] === '0' && result[2][0] === '0' || result[1][0] === '0' && result[1][1] === '0') && result[0].length !== 1) {
+
+  if (result[0][0] == '0' || result[0][1] === '0' && result [2][0] === '0' || result[1][0] === '0' && result [1][1] === '0' && result[0].length !== 1) {
     expectedResult = false;
   }
 
   return expectedResult;
 }
 
+console.log(cryptSolution(crypt, key));
 
-console.log(isCryptSolution(crypt, solution));
 
 
 
